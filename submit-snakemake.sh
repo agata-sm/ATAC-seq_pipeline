4adf8881163b34943aff7834cd79643bf1d62003
agata-sm <smialowskaa@gmail.com>
2019-10-25 13:37:12 +0200

    fixing paths

--- submit-snakemake.sh
+++ submit-snakemake.sh
@@ -2,7 +2,7 @@
 
 # Run snakemake
 snakemake \
-    --snakefile /proj/uppstore2019111/nobackup/private/nbis4833/tst/pipeline/ATAC-seq_pipeline/atacseq_proc_snakefile \
+    --snakefile /proj/uppstore2019111/nobackup/private/nbis4833/analysis/processing/ATAC-seq_pipeline/atacseq_proc_snakefile \
     --rerun-incomplete \
     --jobs 50 \
     --cluster-config cluster.yml \
